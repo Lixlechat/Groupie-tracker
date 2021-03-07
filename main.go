89509800a13117e7b3c93394e9aad7bea8be900b
Sebastian Alonso <sebastian.alonso@ynov.com>
2021-03-07 16:36:20 +0100

    groupie-tracker

--- main.go
+++ main.go
@@ -7,11 +7,12 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"sort"
 	"strconv"
 )
 
 //------------STRUCTURES------------
-
+//----
 type Artist []struct {
 	Id           int      `json:"id"`
 	Image        string   `json:"image"`
@@ -19,12 +20,14 @@ type Artist []struct {
 	Members      []string `json:"members"`
 	CreationDate int      `json:"creationDate"`
 	FirstAlbum   string   `json:"firstAlbum"`
+	Albumyears   int
 }
 
 type Location struct {
 	Index []struct {
-		Id       int      `json:"id"`
-		Location []string `json:"locations"`
+		Id            int      `json:"id"`
+		Location      []string `json:"locations"`
+		LocationClear []string
 	} `json:"index"`
 }
 
@@ -49,6 +52,13 @@ type API struct {
 	Dates    Dates
 	Relation Relation
 }
+type APIFilter struct {
+	ID       int
+	Artist   Artist
+	Location Location
+	Dates    Dates
+	Relation Relation
+}
 
 type OneArtist struct {
 	Artist   Artist
@@ -62,16 +72,156 @@ type Page struct {
 }
 
 type Pagemain struct {
-	Data API
+	Data           API
+	FiltreLocation []string
+	FiltreCreation []string
+	FiltreAlbum    []string
 }
 
 type Artistmain struct {
 	Data OneArtist
 }
+type ReceiveFilter struct {
+	RecLocation string
+	RecCreation string
+	RecAlbum    string
+	RecMembre   string
+}
 
-var sel string
+var filtrecreation []string
+var filtrelocation []string
+var filtreAlbum []string
 var APITracker API
 
+func GetFilter() {
+	for i := range APITracker.Artist {
+		add := ""
+		count := 0
+		for _, j := range APITracker.Artist[i].FirstAlbum {
+			if count == 2 {
+				add += string(j)
+			}
+			if j == '-' {
+				count++
+			}
+		}
+		intyears, _ := strconv.Atoi(add)
+		APITracker.Artist[i].Albumyears = intyears
+		valide := true
+		for _, j := range filtreAlbum {
+			if j == add {
+				valide = false
+			}
+		}
+		if valide == true {
+			filtreAlbum = append(filtreAlbum, add)
+		}
+		strcreation := strconv.Itoa(APITracker.Artist[i].CreationDate)
+		valide = true
+		for _, j := range filtrecreation {
+			if j == strcreation {
+				valide = false
+			}
+		}
+		if valide == true {
+			filtrecreation = append(filtrecreation, strcreation)
+		}
+
+	}
+	sort.Strings(filtreAlbum)
+	sort.Strings(filtrecreation)
+	for i := range APITracker.Location.Index {
+		for j := range APITracker.Location.Index[i].LocationClear {
+			valide := true
+			add := APITracker.Location.Index[i].LocationClear[j]
+			for h := range filtrelocation {
+				if filtrelocation[h] == add {
+					valide = false
+				}
+			}
+			if valide == true {
+				filtrelocation = append(filtrelocation, add)
+			}
+		}
+	}
+	sort.Strings(filtrelocation)
+}
+
+func GetArtistFilter(filtercountry string, filtercreation string, filternbrmember string, filteralbum string) API {
+	TRLocation := false
+	TRCreation := false
+	TRMember := false
+	TRAlbum := false
+
+	creation, _ := strconv.Atoi(filtercreation)
+	nbrmember, _ := strconv.Atoi(filternbrmember)
+	album, _ := strconv.Atoi(filteralbum)
+	var Apifiltre API
+	for i := range APITracker.Artist {
+		TRLocation = false
+		TRCreation = false
+		TRMember = false
+		TRAlbum = false
+		if filtercountry == "" {
+			TRLocation = true
+		} else {
+			for _, j := range APITracker.Location.Index[i].LocationClear {
+				if j == filtercountry {
+					TRLocation = true
+				}
+			}
+		}
+		if filtercreation == "" {
+			TRCreation = true
+		} else if creation == APITracker.Artist[i].CreationDate {
+			TRCreation = true
+		}
+		if filternbrmember == "0" {
+			TRMember = true
+		} else if nbrmember == len(APITracker.Artist[i].Members) {
+			TRMember = true
+		}
+		if filteralbum == "" {
+			TRAlbum = true
+		} else if album == APITracker.Artist[i].Albumyears {
+			TRAlbum = true
+		}
+		if TRLocation == true && TRCreation == true && TRMember == true && TRAlbum == true {
+			Apifiltre.Artist = append(Apifiltre.Artist, APITracker.Artist[i])
+			Apifiltre.Location.Index = append(Apifiltre.Location.Index, APITracker.Location.Index[i])
+			Apifiltre.Dates.Index = append(Apifiltre.Dates.Index, APITracker.Dates.Index[i])
+			Apifiltre.Relation.Index = append(Apifiltre.Relation.Index, APITracker.Relation.Index[i])
+		}
+	}
+	return Apifiltre
+}
+
+//enleve
+func Getbeautiful() {
+	for i := range APITracker.Location.Index {
+		for j := range APITracker.Location.Index[i].Location {
+			valid := true
+			tablocate := []rune(APITracker.Location.Index[i].Location[j])
+			for l := range tablocate {
+				if valid == true {
+					if tablocate[l] == ':' {
+						valid = false
+					}
+					if l == 0 {
+						tablocate[l] -= 32
+					}
+					if tablocate[l] == '-' || tablocate[l] == '_' {
+						tablocate[l] = ' '
+						tablocate[l+1] -= 32
+					}
+				}
+			}
+			APITracker.Location.Index[i].LocationClear = append(APITracker.Location.Index[i].LocationClear, string(tablocate))
+		}
+	}
+}
+
+//on sort les informations de l'api
 func UnmarshalJason(url string, api interface{}) {
 	res, err := http.Get(url)
 	if err != nil {
@@ -89,6 +239,8 @@ func UnmarshalJason(url string, api interface{}) {
 	}
 
 }
+
+//page erreur lorsque l'on trouve pas la reponse avec la searchbar
 func Erreur404(w http.ResponseWriter, r *http.Request) {
 	var tmpl = template.Must(template.ParseFiles("Erreur.html"))
 	p := Page{Valeur: "essaie"}
@@ -99,6 +251,7 @@ func Erreur404(w http.ResponseWriter, r *http.Request) {
 
 }
 
+//page contact avec info developper
 func contact(w http.ResponseWriter, r *http.Request) {
 	var tmpl = template.Must(template.ParseFiles("contact.html"))
 	p := Page{Valeur: "essaie"}
@@ -109,17 +262,7 @@ func contact(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func concertPage(w http.ResponseWriter, r *http.Request) {
-
-	tmpl := template.Must(template.ParseFiles("concerts.html"))
-
-	y, _ := strconv.Atoi(r.URL.Path[8:])
-	fmt.Println(y)
-
-	tmpl.Execute(w, nil)
-
-}
-
+//
 func artistpage(w http.ResponseWriter, r *http.Request) {
 	var dates []string
 	var relation map[string][]string
@@ -131,7 +274,7 @@ func artistpage(w http.ResponseWriter, r *http.Request) {
 	y, _ := strconv.Atoi(r.URL.Path[8:])
 	location = APITracker.Location.Index[y-1].Location
 	OneArtist.Location = location
-	fmt.Println(OneArtist.Location)
+	// fmt.Println(OneArtist.Location)
 	artist = append(artist, APITracker.Artist[y-1])
 	OneArtist.Artist = artist
 	relation = APITracker.Relation.Index[y-1].DatesLocation
@@ -139,26 +282,49 @@ func artistpage(w http.ResponseWriter, r *http.Request) {
 	dates = APITracker.Dates.Index[y-1].Dates
 	OneArtist.Dates = dates
 	P := Artistmain{Data: OneArtist}
-
-	fmt.Println(y)
-
-	fmt.Println(APITracker.Artist[y-1])
-
-	// Search()
-
+	//on met tout dans la variable one artist, on vient chercher les informations dans les strucures nommé,
+	//on les stock dans one artiste avec l'index de l'artiste demander avec Url.Path
+	//tout est stocké dans la structures api, on append le tout pour que toutes les informations apparaise dans la page Artist
+	// et on renvoi le tout sur la page artiste avec le template execute qui contient toute les informations avec OneArtist
 	tmpl.Execute(w, P)
 }
 
 //----PAGE D'ACCEUIL
-
+//Apitracker est la variable qui contient la structure api qui contient elle meme tout les autres structures qui permet de grab toute les informations presente dans l'api
 func mainpage(w http.ResponseWriter, r *http.Request) {
-	P := Pagemain{Data: APITracker}
+	FiltreApi := APITracker
 	tmpl := template.Must(template.ParseFiles("index.html"))
+
+	Filtre := ReceiveFilter{
+		RecLocation: r.FormValue("Location"),
+		RecCreation: r.FormValue("Creationdate"),
+		RecAlbum:    r.FormValue("Albumdate"),
+		RecMembre:   r.FormValue("Nombredemembre"),
+	}
+	fmt.Println("---------------------------")
+	fmt.Println(Filtre.RecLocation)
+	fmt.Println(Filtre.RecCreation)
+	fmt.Println(Filtre.RecMembre)
+	fmt.Println(Filtre.RecAlbum)
+	fmt.Println("---------------------------")
+	if Filtre.RecLocation != "" || Filtre.RecCreation != "" || (Filtre.RecMembre != "0" && Filtre.RecMembre != "") || Filtre.RecAlbum != "" {
+		FiltreApi = GetArtistFilter(Filtre.RecLocation, Filtre.RecCreation, Filtre.RecMembre, Filtre.RecAlbum)
+	}
+	if len(FiltreApi.Artist) == 0 {
+		tmpl1 := template.Must(template.ParseFiles("Erreur.html"))
+		tmpl1.Execute(w, nil)
+		fmt.Println("salut")
+		return
+
+	}
+	P := Pagemain{Data: FiltreApi, FiltreAlbum: filtreAlbum, FiltreCreation: filtrecreation, FiltreLocation: filtrelocation}
+	fmt.Println(FiltreApi)
 
 	tmpl.Execute(w, P)
 
 }
 
+//on sort tout les information de l'url api et les stockons dans APItracker et nous utiliser la fonction unmashall pour les stocker dans la variable Apitracker
 func jsonranger() {
 	urlArtists := "https://groupietrackers.herokuapp.com/api/artists"
 	urlLocation := "https://groupietrackers.herokuapp.com/api/locations"
@@ -170,35 +336,8 @@ func jsonranger() {
 	UnmarshalJason(urlDates, &APITracker.Dates)
 	UnmarshalJason(urlRelation, &APITracker.Relation)
 
-	fmt.Println(APITracker)
+	// fmt.Println(APITracker)
 
-}
-
-func filterM(filterMembers string, Members []string) bool {
-	IFM, _ := strconv.Atoi(filterMembers)
-	if len(Members) == IFM {
-		return true
-	}
-	return false
-}
-
-func filtreA(filtreAlbum, Artistalbum string) bool {
-	PlageAlbum, _ := strconv.Atoi(filtreAlbum)
-	Datestring := Artistalbum
-	date, _ := strconv.Atoi(Datestring)
-
-	if date == PlageAlbum {
-		return true
-	}
-	return false
-}
-
-func FiltrCreation(creationDate int, filtreCreation string) bool {
-	PlageCreation, _ := strconv.Atoi(filtreCreation)
-	if creationDate == PlageCreation {
-		return true
-	}
-	return false
 }
 
 //----MAIN
@@ -206,16 +345,21 @@ func FiltrCreation(creationDate int, filtreCreation string) bool {
 func main() {
 	jsonranger()
 
+	Getbeautiful()
+	GetFilter()
+
+	fmt.Println(filtreAlbum)
+	fmt.Println(filtrecreation)
+
 	fs := http.FileServer(http.Dir("paul"))
 	http.Handle("/paul/", http.StripPrefix("/paul/", fs))
-	http.HandleFunc("/concerts/", concertPage)
 	http.HandleFunc("/artist/", artistpage)
 	http.HandleFunc("/contact/", contact)
 	http.HandleFunc("/", mainpage)
 	http.HandleFunc("/error/", Erreur404)
 	// http.HandleFunc("/search", Search)
 
-	if err := http.ListenAndServe(":8010", nil); err != nil {
+	if err := http.ListenAndServe(":9000", nil); err != nil {
 		log.Fatal(err)
 	}
 }
--- paul/main.js
+++ paul/main.js
@@ -1,29 +1,80 @@
+//*compare la valeur a l'entrée avec les valeurs present dans la datalist pour nous renvoyer a la bonne page
+
 function barrederecherche() {
     var VImput = document.getElementById('search').value;
     var VData = document.getElementById('searchs').options;
     console.log(VData)
-    for(let i=0; i < VData.length; i++) {
+    for (let i = 0; i < VData.length; i++) {
         var VTransformeddata = Rtransformer(VData[i].value.toLowerCase());
-        if(VData[i].value.toLowerCase()== VImput.toLowerCase() || VTransformeddata == VImput.toLowerCase()) {
+        if (VData[i].value.toLowerCase() == VImput.toLowerCase() || VTransformeddata == VImput.toLowerCase()) {
             var ID = VData[i].dataset.id;
-            window.location.href="/artist/"+ID;
+            window.location.href = "/artist/" + ID;
             console.log(ID)
             return;
         }
     }
 
-    // document.location.href="http://localhost:8010/error/";
-    
+    window.location.href = "http://localhost:8010/error/";
+
 }
 console.log("fichierjscharger")
 
+//* transforme les donnée present dans la datalist  pour les recherches taper a la manos. ça supprime le | pour la description
 function Rtransformer(texte) {
     var Ntexte = "";
-    for(i = 0; i < texte.length; i ++) {
-        if(texte[i] == " " && texte[i+1] == "|") {
+    for (i = 0; i < texte.length; i++) {
+        if (texte[i] == " " && texte[i + 1] == "|") {
             return Ntexte;
         } else {
             Ntexte = Ntexte + texte[i];
         }
     }
 }
+
+function boutoncaché() {
+    var otherCheckbox = document.querySelector('input[value="other"]');
+    var otherText = document.querySelector('input[id="otherValue"]');
+    otherText.style.visibility = 'hidden';
+
+    otherCheckbox.onchange = function() {
+        if (otherCheckbox.checked) {
+            otherText.style.visibility = 'visible';
+            otherText.value = '';
+        } else {
+            otherText.style.visibility = 'hidden';
+        }
+    };
+}
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+function openNav() {
+    document.getElementById("mySidebar").style.width = "300px";
+    document.getElementById("main").style.marginLeft = "300px";
+}
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
+
+
